3945f5a1230f11d03b89d63936a06b2e61175af7
Cary Haynie <cary.haynie@gmail.com>
2011-05-25 14:40:10 -0400

    updating install script to setup git ( since I always forget. )

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@
 # setup this account's vim files to point to the ones in this repository.
 ln -s ./vim/ ~/.vim
 ln -s .vimrc ~/.vimrc
+
+# setup git correctly
+git config --global user.name "Cary Haynie"
+git config --global user.email cary.haynie@gmail.com
